fddbd862a85ea83a6e4b74330566156813562bd8
Mirio Eggmann <mirio.eggmann@protonmail.ch>
2019-10-02 13:17:46 +0200

    added some commands for global git config

---
+++ git/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git config --global user.name "Mirio Eggmann"
+git config --global user.email "mirio.eggmann@protonmail.ch"
+git config --global gpg.program gpg2
+git config --global user.signingkey 5073994D0489F584
+git config --global commit.gpgsign true
+git config --global tag.gpgsign true
